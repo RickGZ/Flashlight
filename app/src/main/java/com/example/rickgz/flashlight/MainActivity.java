22b6ce1fb5f0911f51c3ad23356e1a54d515fb17
RickGZ <rickgz66@gmail.com>
2017-07-04 20:22:13 +0200

    added button method

--- app/src/main/java/com/example/rickgz/flashlight/MainActivity.java
+++ app/src/main/java/com/example/rickgz/flashlight/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rickgz.flashlight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.flashlightButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
